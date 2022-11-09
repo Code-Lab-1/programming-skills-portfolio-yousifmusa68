a9824f5a0ce6a7434f049923e9c42775a334da2d
yousifmusa <yousifmusa68@gmail.com>
2022-11-09 13:32:57 +0400

    Create exercise7.py

---
+++ Chapter 3- Data Structures/exercise7.py
@@ -0,0 +1,18 @@
+print (sorted(locations))
+
+print("\noriginal order:")
+print(locations)
+
+print("\nReverse alphabetical:")
+print(sorted(locations, reverse=True))
+
+print("\nOriginal order:")
+print(locations)
+
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+print("\nOriginal order:")
+locations.reverse()
+print(locations)
